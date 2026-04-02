6eb9382a852ccf2c9ca75b514aea7ca4714ef814
Jeff <tabirijeffrey@gmail.com>
2026-04-02 15:41:29 +0200

    feat: read vertex information of obj file

--- main.c
+++ main.c
@@ -1,12 +1,10 @@
-
 #include "objloader.h"
 #include <stdio.h>
+
 int main(int argc, char *argv[]) {
   char *path = "cube.obj";
 
   read_file(path);
-
-  printf("%s\n", path);
 
   return 1;
 }
--- objloader.h
+++ objloader.h
@@ -2,18 +2,44 @@
 #define VIOLA_OBJ_LOADER_H
 
 #include <stdio.h>
-static void read_file(char *path) {
-  int n = 100;
+
+float vertices[4096] = {};
+
+static int read_file(char *path, int *vertices) {
+  int n = 4096;
   char line[n];
 
   FILE *fptr;
 
   fptr = fopen(path, "r");
 
-  while (fgets(line, n, fptr)) {
-
-    fclose(fptr);
+  if (fptr == NULL) {
+    printf("Error reading file.");
   }
+
+  int vertex = 0;
+
+  while (fgets(line, n, fptr)) {
+    // Process vertex normals
+    if (line[0] == 'v' && line[0] == 'n') {
+    }
+
+    // Handle vertex
+    if (line[0] == 'v' && line[1] == ' ') {
+      float x = 0.0f;
+      float y = 0.0f;
+      float z = 0.0f;
+      sscanf((line + 1), "%f%f%f", &x, &y, &z);
+      vertices[vertex] = x;
+      vertices[vertex] = y;
+      vertices[vertex] = z;
+      vertex += 3;
+    }
+  }
+
+  fclose(fptr);
+
+  printf("vertex: %d", vertex);
 }
 
 #endif // !VIOLA_OBJ_LOADER_H
